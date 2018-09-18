8fe76beb3f6d07a709e7e26bcaa675d03172496f
Artur Kuchynski <archeski.dk@gmail.com>
2018-09-18 13:25:05 +0300

    do refactoring, remove rss parsing

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from parsers.weather_parser import WeatherParser
+import argparse
+import sys
 import re
+from pathlib import Path
 
 
 def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--path", required=False,
+                    help="path to .xlsx file where the weather data will be stored")
+    args = vars(ap.parse_args())
+    file = Path(args["path"]).absolute() if args["path"] else None
+
+    if file and not file.exists():
+        print('File not found.\nPlease check if the file path was set correctly.')
+        sys.exit(1)
+
     while True:
         city = input("Enter city name (latin chars only): ").lower()
 
         if re.match("[a-z]", city):
             break
 
-    wp = WeatherParser()
-
     print("Processing your request...")
-    wp.get_forecast_local_today()
-    wp.get_forecast_rss_today(city)
+    wp = WeatherParser(file)
     wp.get_forecast_decade(city)
-    print("DONE!\nCheck the output file!")
+    print("DONE!")
 
 
 if __name__ == "__main__":
--- parsers/weather_parser.py
+++ parsers/weather_parser.py
@@ -1,137 +1,123 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas
+import sys
+import xlsxwriter
 
 
 class WeatherParser:
 
-    @staticmethod
-    def get_forecast_local_today():
+    def __init__(self, file):
+        self.file = file
+
+    def _get_content(self, url):
         """
-        Gets the local weather forecast and saves It to .txt file
+        Grab the content from web page
+        :param url: meteo.by site url
+        :return: page content as BeautifulSoup object
         """
-        page = requests.get('https://meteo.by/')
+        try:
+            page = requests.get(url)
+            page.raise_for_status()
+            if page.status_code < 307:
+                return BeautifulSoup(page.content, 'lxml')
+            else:
+                print('Error: {0}: {1} for url: {2}'.format(page.status_code, page.reason, page.url))
+                sys.exit(1)
 
-        # success
-        if page.ok:
-            soup = BeautifulSoup(page.content, 'lxml')
-            data = {}
+        except requests.exceptions.RequestException as ex:
+            print("Error: " + str(ex))
+            sys.exit(1)
 
-            # get the current date
-            p = soup.find('p', {"class": "c"})
-            data["Город"] = p.select("strong")[0].text
-            data["Дата"] = p.select("small")[0].text
-            data["Время"] = p.select("small")[1].text
+    def get_forecast_decade(self, city):
+        """
+        Get the weather forecast for one decade
+        :param city: string as city name for which the forecast data will be parsed
+        """
+        soup = self._get_content('https://meteo.by/{0}/'.format(city))
 
-            # get local temperature
-            p = soup.find('p', {"class": 't'})
+        # grab the forecast data from html tabs
+        forecast_data = [[data.text.strip() for data in row_data.select('td')]
+                         for row_data in soup.find_all('tr', {'class': 'time'})]
+
+        for data in forecast_data:
             # remove whitespaces
-            data["Температура"] = p.select("strong")[0].text.strip().replace('\r\n                        ', '')
+            data[0] = ''.join(data[0].split())
 
-            # get forecast
-            p = soup.find('p', {"class": 's'})
-            data["Погода"] = p.text.strip()
+        dates = soup.find_all('p', {'class': 'date'})
+        date_list, temp = [], {}
 
-            df = pandas.DataFrame.from_dict(data, orient='index', columns=['Прогноз для:'])
+        # fill the date list
+        for date in dates:
+            temp["Date"] = date.select("strong")[0].text
+            temp["Month"] = date.text[4:]
+            temp["Day"] = date.select("span")[0].text
+            date_list.append(" ".join(list(temp.values())))
 
-            with open("out/local.txt", "w") as file:
-                file.write(df.to_string())
+        dates = date_list
 
+        if self.file:
+            self._to_xl(dates, forecast_data, self.file)
         else:
-            # if page rendered with an error code, save the error code
-            if page.status_code >= 400:
-                err_message = "Error " + str(page.status_code)
-                print(err_message)
-                with open("out/local.txt", "w") as file:
-                    file.write(err_message)
+            self._to_cli(dates, forecast_data)
 
-    @staticmethod
-    def get_forecast_rss_today(city):
+    def _to_cli(self, dates, forecast):
         """
-        Gets the weather forecast from rss feed for given city
-
-        :param city: string as city name for which the forecast data will be parsed
+        Print retrieved forecast data on the console
+        :param dates: list of decade dates
+        :param forecast: list of lists of forecast data for each time of day for one decade
         """
-        page = requests.get('https://meteo.by/{0}/rss/'.format(city))
+        day_index, temp_index = 0, 0
+        for quarter_index in range(0, len(forecast), 4):
+            print("\n" + dates[day_index])
+            day_index += 1
+            if quarter_index > 0:
+                for index in range(temp_index, quarter_index):
+                    print(str(forecast[index]))
 
-        if page.ok:
-            soup = BeautifulSoup(page.text, 'lxml')
-            td = [[data.text.strip() for data in row_data.select('td')]
-                  for row_data in soup.find_all('tr')[2:-1:]]
+            temp_index = quarter_index
 
-            for data in td:
-                # remove whitespaces
-                data[0] = data[0].replace("\r\n                            ", ' ')
-
-            df = pandas.DataFrame(td,
-                                  columns=['Температура',
-                                           'Прогноз',
-                                           'Давление',
-                                           'Влажность',
-                                           'Ветер',
-                                           'Направление'])
-
-            # serialize the data
-            df.to_csv("out/{0}_rss.csv".format(city))
-            with open("out/{0}_rss.txt".format(city), "w") as file:
-                file.write(df.to_string())
-
-
-        else:
-            if page.status_code >= 400:
-                err_message = "Error " + str(page.status_code)
-                print(err_message)
-                with open("out/{0}_rss.txt".format(city), "w") as file:
-                    file.write(err_message)
-
-    @staticmethod
-    def get_forecast_decade(city):
+    def _to_xl(self, dates, forecast, path):
         """
-        Get the weather forecast for the one decade for given city
-
-        :param city: string as city name for which the forecast data will be parsed
+        Write retrieved forecast data in .xlsx file
+        :param dates: list of decade dates
+        :param forecast: list of lists of forecast data for each time of day for one decade
+        :param path: .xlsx file path
         """
-        page = requests.get('https://meteo.by/{0}/'.format(city))
+        workbook = xlsxwriter.Workbook(path)
+        worksheet = workbook.add_worksheet()
 
-        if page.ok:
-            soup = BeautifulSoup(page.content, 'lxml')
+        worksheet.set_column(0, 5, 25)
+        bold = workbook.add_format({'bold': 1})
 
-            td = [[data.text.strip() for data in row_data.select('td')]
-                  for row_data in soup.find_all('tr', {'class': 'time'})]
+        criteria = ['Температура min..max',
+                    'Погода',
+                    'Давление min..max',
+                    'Влажность min..max',
+                    'Ветер min..max',
+                    'Направление']
 
-            for data in td:
-                # remove whitespaces
-                data[0] = data[0].replace("\r\n                            ", ' ')
+        row, col = 0, 0
+        for i in range(0, len(criteria)):
+            worksheet.write_string(row, col + i, criteria[i], bold)
 
-            dates = soup.find_all('p', {'class': 'date'})
+        day_index, temp_index, row = 0, 0, 1
 
-            date_list, temp = [], {}
+        # loop through the forecast data
+        for quarter_index in range(0, len(forecast), 4):
+            # write the date for each day
+            worksheet.write_string(row, 0, dates[day_index])
 
-            for date in dates:
-                temp["Date"] = date.select("strong")[0].text
-                temp["Month"] = date.text[4:]
-                temp["Day"] = date.select("span")[0].text
+            if quarter_index > 0:
+                # loop through the forecast for each time of day
+                for index in range(temp_index, quarter_index):
+                    row += 1
+                    col = 0
+                    for item in forecast[index]:
+                        # write the data for each forecast criterion
+                        worksheet.write_string(row, col, item)
+                        col += 1
+                temp_index = quarter_index
+                day_index += 1
+                row += 2
 
-                date_list.append(" ".join(list(temp.values())))
-
-            df = pandas.DataFrame(td, columns=['Температура',
-                                               'Прогноз',
-                                               'Давление min…max',
-                                               'Влажность min…max',
-                                               'Ветер',
-                                               'Направление'])
-
-            with open("out/{0}_decade.txt".format(city), "w") as file:
-                i = 0
-                # group weather data for each day according to the time of day
-                for n in range(0, len(df), 4):
-                    file.write(str(date_list[i]) + "\n")
-                    file.write(df[n:n + 4:].to_string() + "\n\n")
-                    i += 1
-
-        else:
-            if page.status_code >= 400:
-                err_message = "Error " + str(page.status_code)
-                print(err_message)
-                with open("out/{0}_rss.txt".format(city), "w") as file:
-                    file.write(err_message)
+        workbook.close()
